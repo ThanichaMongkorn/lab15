977a9c614ceaef1e0881e7e7311289f61c9ba374
= <=>
2023-01-17 15:54:58 +0700

    lab15.1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for(int i=1; i<N; i++){
+        for(int j=i; j>0; j--)
+            if(d[j]>d[j-1]){
+                double temp = d[j];
+                d[j] = d[j-1];
+                d[j-1] = temp;
+        }
+        cout << "Pass "<< i <<":";
+            for(int i = 0; i < 10; i++) cout << d[i] << " ";
+            cout <<"\n";
+    }
 }
 
 int main(){
