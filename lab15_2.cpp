842386988f6e0c51ce9ed9d156b4b21a9da556af
= <=>
2023-01-17 16:57:41 +0700

    lab15.2

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,37 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool b[][N]){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cout<< b[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+void inputMatrix(double a[][N]){
+    for(int i=0; i<N; i++){
+        cout << "Row "<< i+1<<": ";
+        for(int j=0; j<N; j++){
+            cin >> a[i][j];
+        }
+    
+    }
+}
+void findLocalMax(const double a[][N], bool b[][N]){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            b[i][j]=0;
+        }
+    }
+    for(int i=1; i<N-1; i++){
+        for(int j=1; j<N-1; j++){
+            if(a[i][j]>=a[i-1][j]and a[i][j]>=a[i+1][j]and a[i][j]>=a[i][j-1]and a[i][j]>=a[i][j+1]){
+                b[i][j]=1;   
+            }else{
+                b[i][j]=0;
+            
+            }
+        }
+    }
+}
